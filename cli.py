be97fa495c80f7d9a378a6eda8080f015e20e4de
Coby Lu <lucoby@gmail.com>
2020-12-26 14:28:33 -0500

    Add fastapi server and template for generating pdf of sight words

---
+++ cli.py
@@ -0,0 +1,6 @@
+from clize import run
+from sight_words.sight_words import make_pdf
+
+
+if __name__ == "__main__":
+    run(make_pdf)
--- sight_words/sight_words.py
+++ sight_words/sight_words.py
@@ -1,4 +1,3 @@
-from clize import run
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfbase.ttfonts import TTFont
@@ -11,20 +10,18 @@ def make_pdf(*words):
     :param words: List of words to add to pdf
     """
     height, width = letter  # swapped for landscape
-    c = canvas.Canvas("sight-words.pdf", pagesize=(width, height))
+    filename = "sight-words.pdf"
+    c = canvas.Canvas(filename, pagesize=(width, height))
     pdfmetrics.registerFont(TTFont("Comic Sans", "resources/fonts/comic.ttf"))
 
     for word in words:
         add_word(c, word, width, height)
 
     c.save()
+    return filename
 
 
 def add_word(canvas, word, width, height):
     canvas.setFont("Comic Sans", 72)
     canvas.drawCentredString(width / 2, height / 2, word)
     canvas.showPage()
-
-
-if __name__ == "__main__":
-    run(make_pdf)
