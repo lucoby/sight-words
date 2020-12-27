37d64f57b2f71401b2dbef279a0c80680515fe19
Coby Lu <lucoby@gmail.com>
2020-12-27 09:01:01 -0500

    add options for adding title slide

--- sight_words/sight_words.py
+++ sight_words/sight_words.py
@@ -4,15 +4,24 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 
 
-def make_pdf(*words):
+def make_pdf(*words, title="", subtitle=""):
     """Makes a pdf with one word on each page
 
     :param words: List of words to add to pdf
+    :param title: Adds a title slide with this as the title
+    :param subtitle: Adds this as a subtitle - only used if title is also included
     """
     height, width = letter  # swapped for landscape
     filename = "sight-words.pdf"
     c = canvas.Canvas(filename, pagesize=(width, height))
     pdfmetrics.registerFont(TTFont("Comic Sans", "resources/fonts/comic.ttf"))
+
+    if title != "":
+        c.setFont("Comic Sans", 72)
+        c.drawCentredString(width / 2, height / 2, title)
+        c.setFont("Comic Sans", 48)
+        c.drawCentredString(width / 2, height / 2 - 50, subtitle)
+        c.showPage()
 
     for word in words:
         add_word(c, word, width, height)
